97e18a5d859a02d48b7c4b66407462eabc61e4f7
Mr.N0BODY <51495170+N0BODY2@users.noreply.github.com>
2021-10-27 02:05:42 +0200

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -6,6 +6,17 @@ if Config.ESXtype == 'Old' then
 			Citizen.Wait(0)
 		end
 	end)
+elseif Config.ESXtype == 'New' then	
+    ESX = exports['es_extended']:getSharedObject()
+
+    if not IsDuplicityVersion() then 
+        AddEventHandler('esx:setPlayerData', function(key, val, last)
+            if GetInvokingResource() == 'es_extended' then
+                ESX.PlayerData[key] = val
+                if OnPlayerData ~= nil then OnPlayerData(key, val, last) end
+            end
+        end)
+    end	
 end
 
 local wasinshop 
